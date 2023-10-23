61f27aa791cc7f3805975a2dd19228653990c652
Charlotte Appenzeller <charlotte@appenzeller.net>
2023-10-22 18:03:48 -0700

    moved test data into subdirectory

--- helper.py
+++ helper.py
@@ -3,20 +3,29 @@
 
 import openai
 import whisper
+import dotenv
+import os
 
+# take environment variables from .env.
+dotenv.load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# This function takes a user prompt and returns a chat response
+# It uses the OpenAI API to generate the response
 def chat(prompt):
-    
     completion = openai.ChatCompletion.create(
         model="gpt-4", 
         messages=[{"role": "user", "content": prompt}]
     )
     return completion.choices[0].message.content
 
-
 model = whisper.load_model("tiny")
-result = model.transcribe("isabelle.aifc")
+result = model.transcribe("test/isabelle.aifc")
 text = result["text"]
+print("\n--- Whisper Transcript -----------------")
+print(text)
 
-prompt = f"Summarize the following text:{text}"
+prompt = f"What is she saying and how could i respond:{text}"
 result = chat(prompt)
+print("\n--- LLM Summary -----------------")
 print(result)
